4bdc49c8f17e99e9fd8ab502cb56dda9c11c17fe
Amey <ameynb13@gmail.com>
2025-08-23 22:23:11 +0530

    add skill request endpoints

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { loginUser, registerUser, forgotPassword, resetPassword } = require('../controllers/authController');
 
-router.post('/login', loginUser);
+router.post('/login', loginUser); 
 router.post('/register', registerUser);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password/:token', resetPassword);
